8f447321ea4d89510c3535b9c39278b8bbb9c95f
Kiyo <kiyo@ncat.cafe>
2026-04-02 15:52:36 -0700

    wrap server nix bin and set XKB_CONFIG_ROOT
    taken from https://github.com/StardustXR/server/pull/44/

--- nix/stardust-xr-server.nix
+++ nix/stardust-xr-server.nix
@@ -5,8 +5,10 @@
   vulkan-loader,
   vulkan-headers,
   libxkbcommon,
+  xkeyboard_config,
 
   pkg-config,
+  makeWrapper,
 
   openxr-loader,
   wayland,
@@ -24,10 +26,14 @@ rustPlatform.buildRustPackage rec {
     patchelf $out/bin/stardust-xr-server --add-rpath ${vulkan-loader}/lib
     patchelf $out/bin/stardust-xr-server --add-rpath ${openxr-loader}/lib
     patchelf $out/bin/stardust-xr-server --add-rpath ${libxkbcommon}/lib
+
+    wrapPrograrm $out/bin/stardust-xr-server \
+      --set XKB_CONFIG_ROOT "${xkeyboard_config}/share/X11/xkb"
   '';
 
   nativeBuildInputs = [
     pkg-config
+    makeWrapper
   ];
   buildInputs = [
     vulkan-loader
